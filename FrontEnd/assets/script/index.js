d051b038054f93085e2774918299f860a7b50d9f
azorophelie <azor.ophelie@yahoo.fr>
2024-04-15 16:42:39 +0200

    Ajout de la récupération des travaux et des catégories depuis l'API

--- FrontEnd/assets/script/index.js
+++ FrontEnd/assets/script/index.js
@@ -13,21 +13,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Parcourir les données récupérées et ajouter à la galerie
             data.forEach(work => {
-                // Créer les éléments HTML pour chaque travail
+                
                 const figure = document.createElement('figure');
                 const img = document.createElement('img');
                 const figcaption = document.createElement('figcaption');
 
-                // Ajouter les attributs et le contenu aux éléments
+                
                 img.src = work.imageUrl;
                 img.alt = work.title;
                 figcaption.textContent = work.title;
 
-                // Ajouter les éléments à la figure
+                
                 figure.appendChild(img);
                 figure.appendChild(figcaption);
 
-                // Ajouter la figure à la galerie
+                
                 gallery.appendChild(figure);
             });
         })
@@ -35,4 +35,68 @@ document.addEventListener('DOMContentLoaded', function() {
             // Gérer les erreurs de réseau ou de réponse de l'API
             console.error(error);
         });
+        
 });
+
+
+    // Récupération des catégories depuis le back-end
+    fetch('http://localhost:5678/api/categories')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erreur de réseau.');
+        }
+        return response.json();
+    })
+    .then(categories => {
+        const filterButtonsContainer = document.querySelector('#filter-buttons');
+
+        // Fonction pour appliquer les styles aux boutons et gérer le survol
+        function applyButtonStyles(button) {
+            button.classList.add('filter-button');
+            button.style.borderRadius = '20px'; 
+            button.style.borderWidth = '1px'; 
+            button.style.borderColor = '#1D6154'; 
+            button.style.padding = '5px 15px'; 
+            button.style.backgroundColor = 'white'; 
+            button.style.color = '#1D6154'; 
+            button.style.marginRight = '10px';
+            button.style.fontSize = '16px';
+            
+            
+            button.addEventListener('mouseover', function() {
+                button.style.backgroundColor = '#1D6154'; 
+                button.style.color = 'white'; 
+            });
+
+            button.addEventListener('mouseout', function() {
+                button.style.backgroundColor = 'white'; 
+                button.style.color = '#1D6154'; 
+            });
+        }
+
+        
+        const allButton = document.createElement('button');
+            allButton.textContent = 'Tous';
+            applyButtonStyles(allButton); 
+            allButton.style.padding = '5px 25px'; 
+            filterButtonsContainer.appendChild(allButton);
+            
+        // Génération des boutons pour chaque catégorie
+        categories.forEach(category => {
+            const button = document.createElement('button');
+            button.textContent = category.name;
+            applyButtonStyles(button); 
+            filterButtonsContainer.appendChild(button);
+        });
+        
+        filterButtonsContainer.style.display = 'flex';
+        filterButtonsContainer.style.justifyContent = 'center';
+        filterButtonsContainer.style.marginBottom = '20px';
+        
+    })
+
+    .catch(error => {
+        console.error('Erreur lors de la récupération des catégories:', error);
+    });
+
+    
