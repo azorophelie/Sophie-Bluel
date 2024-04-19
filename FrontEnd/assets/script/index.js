417f054ba76d93763094c8e1f7ac2107c5159605
azorophelie <azor.ophelie@yahoo.fr>
2024-04-19 22:37:12 +0200

    Ajout de fonctionnalité de filtres dynamiques

--- FrontEnd/assets/script/index.js
+++ FrontEnd/assets/script/index.js
@@ -1,45 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('http://localhost:5678/api/works')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erreur de réseau.');
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Suppression des travaux statiques de la galerie
-            const gallery = document.querySelector('.gallery');
-            gallery.innerHTML = ''; 
+/* Fonction pour initialiser la galerie */
 
-            // Parcourir les données récupérées et ajouter à la galerie
-            data.forEach(work => {
-                
-                const figure = document.createElement('figure');
-                const img = document.createElement('img');
-                const figcaption = document.createElement('figcaption');
+function initGallery(data) {
+    const galleryElement = document.querySelector('.gallery');
+  
+    for (const objet of data) {
+        const figure = document.createElement("figure");
+        figure.classList.add("figure");
+        figure.dataset.categoryId = objet.category.id; 
+        galleryElement.appendChild(figure);
 
-                
-                img.src = work.imageUrl;
-                img.alt = work.title;
-                figcaption.textContent = work.title;
+        const image = new Image();
+        image.src = objet.imageUrl;
+        image.alt = objet.title;
+        figure.appendChild(image);
 
-                
-                figure.appendChild(img);
-                figure.appendChild(figcaption);
+        const figcaption = document.createElement("figcaption");
+        figcaption.textContent = objet.title;
+        figure.appendChild(figcaption);
+    } 
+}
 
-                
-                gallery.appendChild(figure);
-            });
-        })
-        .catch(error => {
-            // Gérer les erreurs de réseau ou de réponse de l'API
-            console.error(error);
-        });
-        
-});
+/* Fonction pour filtrer les projets par catégorie */
 
 function filterByCategory(categoryId) {
-    const galleryItems = document.querySelectorAll('.gallery-item');
+    const galleryItems = document.querySelectorAll('.figure');
     galleryItems.forEach(item => {
         const itemCategoryId = item.dataset.categoryId;
         if (itemCategoryId === categoryId || categoryId === '0') {
@@ -50,20 +34,47 @@ function filterByCategory(categoryId) {
     });
 }
 
+/* Récupérer les données des travaux depuis l'API */
 
-function filterByCategory(categoryId) {
-    const galleryItems = document.querySelectorAll('.gallery-item');
-    galleryItems.forEach(item => {
-        const itemCategoryId = item.dataset.categoryId;
-        if (itemCategoryId === categoryId || categoryId === '0') {
-            item.style.display = ''; 
-        } else {
-            item.style.display = 'none';
+fetch('http://localhost:5678/api/works')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erreur de réseau.');
         }
+        return response.json();
+    })
+    .then(data => {
+        initGallery(data); 
+    })
+    .catch(error => {
+        console.error('Error fetching works data:', error);
     });
-}
 
-// Fonction pour filtrer les projets en fonction de la catégorie
+    function applyButtonStyles(button) {
+        button.classList.add('filter-button');
+        button.style.borderRadius = '20px'; 
+        button.style.borderWidth = '1px'; 
+        button.style.borderColor = '#1D6154'; 
+        button.style.padding = '5px 15px'; 
+        button.style.backgroundColor = 'white'; 
+        button.style.color = '#1D6154'; 
+        button.style.marginRight = '10px';
+        button.style.fontSize = '16px';
+        
+        
+        
+        button.addEventListener('mouseover', function() {
+            button.style.backgroundColor = '#1D6154'; 
+            button.style.color = 'white'; 
+        });
+
+        button.addEventListener('mouseout', function() {
+            button.style.backgroundColor = 'white'; 
+            button.style.color = '#1D6154'; 
+        });
+    }
+    
+// Récupérer les données des catégories depuis l'API
 fetch('http://localhost:5678/api/categories')
     .then(response => {
         if (!response.ok) {
@@ -74,52 +85,53 @@ fetch('http://localhost:5678/api/categories')
     .then(categories => {
         const filterButtonsContainer = document.querySelector('#filter-buttons');
 
-        // Fonction pour appliquer les styles aux boutons et gérer le survol
-        function applyButtonStyles(button) {
-            button.classList.add('filter-button');
-            button.style.borderRadius = '20px'; 
-            button.style.borderWidth = '1px'; 
-            button.style.borderColor = '#1D6154'; 
-            button.style.padding = '5px 15px'; 
-            button.style.backgroundColor = 'white'; 
-            button.style.color = '#1D6154'; 
-            button.style.marginRight = '10px';
-            button.style.fontSize = '16px';
-            
-            
-            button.addEventListener('mouseover', function() {
-                button.style.backgroundColor = '#1D6154'; 
-                button.style.color = 'white'; 
-            });
-
-            button.addEventListener('mouseout', function() {
-                button.style.backgroundColor = 'white'; 
-                button.style.color = '#1D6154'; 
+        
+        // Fonction pour gérer le clic sur les boutons de filtre
+    
+        function handleClick(categoryId, filterButtons) {
+            const allFilterButtons = document.querySelectorAll(".filter-button");
+            filterByCategory(categoryId);
+            allFilterButtons.forEach(currentFilterButton => {
+                if (currentFilterButton === filterButtons) {
+                    currentFilterButton.classList.add("active-button");
+                } else {
+                    currentFilterButton.classList.remove("active-button");
+                }
             });
         }
+       
 
-        
+        /*Créer le bouton 'Tous'*/
+
         const allButton = document.createElement('button');
         allButton.textContent = 'Tous';
         allButton.id = '0';
-        applyButtonStyles(allButton); 
-        allButton.style.padding = '5px 25px'; 
+        allButton.classList.add("active-filter-btn");
+        applyButtonStyles(allButton);
+        allButton.style.padding = '5px 25px';
+        allButton.style.backgroundColor = '#1D6154'; 
+        allButton.style.color = 'white';
+        allButton.addEventListener('click', () => handleClick('0', allButton));
         filterButtonsContainer.appendChild(allButton);
             
-        // Génération des boutons pour chaque catégorie
-        categories.forEach((category, index) => {
+        // Générer les boutons pour chaque catégorie
+
+        categories.forEach(category => {
             const button = document.createElement('button');
             button.textContent = category.name;
-            button.id = (index + 1).toString();
-            applyButtonStyles(button); 
+            button.id = category.id.toString();
+            button.classList.add('filter-button');
+            applyButtonStyles(button);
+            button.addEventListener('click', () => handleClick(category.id.toString(), button));
             filterButtonsContainer.appendChild(button);
         });
         
         filterButtonsContainer.style.display = 'flex';
         filterButtonsContainer.style.justifyContent = 'center';
         filterButtonsContainer.style.marginBottom = '20px';
+
         
     })
     .catch(error => {
-        console.error('Erreur lors de la récupération des catégories:', error);
+        console.error('Error fetching categories data:', error);
     });
